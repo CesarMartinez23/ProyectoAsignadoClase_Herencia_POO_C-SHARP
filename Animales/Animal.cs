1f045a7ffbdda6300df98e43042d3bbc26079f6f
Cesar Martinez <oliver-cesar@hotmail.com>
2021-10-01 20:27:53 -0600

    Creacion de Clases y herencia, metodos de la clase padre (Animal).

---
+++ Animales/Animal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animales
+{
+
+    // Class Father
+    class Animal
+    {
+        // Create Properties
+
+        private string _foto { get; set; }
+        private string _comida { get; set; }
+        private string _localizacion { get; set; }
+        private string _tamano { get; set; }
+
+
+        // Create Methods
+
+        public string hacerRuido()
+        {
+            return "";
+        }
+
+        public string comer()
+        {
+            return "";
+        }
+
+        public string dormir()
+        {
+            return "";
+        }
+
+        public string rugir()
+        {
+            return "";
+        }
+    }
+}
